d65554ee22bbaf4416899915dde9517a14a946cb
olumuyiwa_kolayemi <kolayemi.olumuyiwa@gmail.com>
2020-11-10 10:42:29 -0500

    I modified the main code to return only the last INSDSequence as requested in the lab

--- AWS_Lab5.py
+++ AWS_Lab5.py
@@ -1,6 +1,6 @@
 #Import os module
 import os
-#from os.path import abspath
+from os.path import abspath
 import xml.etree.ElementTree as ET
 rawData = ('c:/users/19293/Documents/Git Project/rawdata.xml')
 output_file = ('c:/users/19293/Documents/Git Project/output_file.txt')
@@ -12,14 +12,18 @@ data_in = open(rawData, "r")
 
 tree = ET.parse('c:/users/19293/Documents/Git Project/rawdata.xml')
 root = tree.getroot()
-print(root.tag)
+#print(root.tag)
 
+"""
 for child in root:
     print(child.tag, child.attrib)
     data_out.write("%s" % child.text)
 data_out.close()
+"""
 
-"""
-for x in range(0, 3):
-    print(root[0][x].text)
-"""
+for child in root:
+    if child.tag == "INSDSeq_sequence":
+        print(child.text)
+        data_out.write("%s" % child.text)
+data_out.close()
+
